f6a704fe369825f4d09f18f97ff8bc7961f55166
sdkevorkian <sdkevorkian@gmail.com>
2017-04-06 19:33:53 -0700

    added some fun stuff, animations sounds and time to finish display

--- starter-code/main.js
+++ starter-code/main.js
@@ -9,22 +9,35 @@ function onLoad() {
     var blueGameDivs = document.querySelectorAll(".flex-container")[1].getElementsByClassName("box");
     var redPosition = 0; // 0 is index of 1st box in redGameDivs
     var bluePosition = 0; // likewise for blueGameDivs
+    var audioBeercan = document.getElementById("beercan");
+    var audioManBurp = document.getElementById("manburp");
 
-    function gameOver(player) {
+    function playAudioGameOver() {
+        audioBeercan.curentTime += 650;
+        audioBeercan.play();
+        setTimeout(function() { audioManBurp.play() }, 2000);
+    }
+
+    function gameOver(player, time, timeStarted) {
         message.classList.remove("hidden");
-        message.querySelector("h1").innerHTML = `${player} got the beer!!`;
+        playAudioGameOver();
+        var timeToFinish = (time - timeStarted) / 1000;
+        message.querySelector("h1").innerHTML = `${player} got the beer!! in ${timeToFinish} seconds!!`;
+        message.querySelector("h1").classList.add("party");
     }
 
 
     message.addEventListener("click", function() {
+        // removes start menu and begins game
         message.classList.add("hidden");
         gameArea.classList.remove("hidden");
-        var gameWon = false; // designnates game hasn't been won yet, when true the key ups wont do anything
-
+        var gameWon = false;
+        var timeStarted = Date.now(); //set to Date.Now() now, will subtract from Date.Now() after to obtain time to finish
+        console.log(timeStarted);
         document.addEventListener("keyup", function(e) {
-            // the first two if statments check if the current character has reached the end of it's lane
-            // AND that the game has ended (if so, key presses won't do anything)
 
+
+            // if game has been won, nothing happens with key presses
             if (gameWon === false) {
                 if (e.key === "a") {
                     // if the corect key is pressed, the current div's red/blue class is removed
@@ -36,7 +49,7 @@ function onLoad() {
                     redGameDivs[redPosition].classList.add("redBox");
                     if (redPosition === redGameDivs.length - 1) {
                         gameWon = true;
-                        gameOver("Boomhauer");
+                        gameOver("Boomhauer", Date.now(), timeStarted);
                     }
                 } else if (e.key === "l") {
                     // "l" is assigned to blueBox AKA Bill
@@ -45,7 +58,7 @@ function onLoad() {
                     blueGameDivs[bluePosition].classList.add("blueBox");
                     if (bluePosition === blueGameDivs.length - 1) {
                         gameWon = true;
-                        gameOver("Bill");
+                        gameOver("Bill", Date.now(), timeStarted);
                     }
                 }
             }
